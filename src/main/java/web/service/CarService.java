48031d3742af8d1197d07ebc4b875c798655041e
Artemiy Bobritskiy <artemiy.bobritskiy@mail.ru>
2026-01-23 19:49:59 +0300

    Add CarService interface

--- src/main/java/web/service/CarService.java
+++ src/main/java/web/service/CarService.java
@@ -1,29 +1,9 @@
 package web.service;
 
-import org.springframework.stereotype.Service;
 import web.model.Car;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
-@Service
-public class CarService {
-
-    private final List<Car> cars = Arrays.asList(
-            new Car("Toyota", "GR86", "S866CW"),
-            new Car("Lexus", "LFA", "A433GT"),
-            new Car("Volvo", "S60", "N703BT"),
-            new Car("Porsche", "911", "N001SW"),
-            new Car("Audi", "R8", "S350NC")
-    );
-
-    public List<Car> getCars(int count) {
-        if (count >= 5) {
-            return new ArrayList<>(cars);
-        } else if (count < 1) {
-            return new ArrayList<>();
-        }
-        return new ArrayList<>(cars.subList(0, count));
-    }
+public interface CarService {
+    public List<Car> getCars(int count);
 }
